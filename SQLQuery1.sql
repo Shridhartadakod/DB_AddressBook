083e9c8d549f6d01e97c4ea3a93fcb63f4747b34
Shridhartadakod <shridhar1491998@gmail.com>
2022-03-25 11:29:54 +0530

    count is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -45,3 +45,5 @@ Update AddressBook set Type = 'Friends' where FirstName='harish';
 Update AddressBook set Type = 'Professional' where FirstName='Venkatesh';
 Update AddressBook set Type = 'Family' where FirstName='Dhanshree';
 Alter Table AddressBook drop Column ContactType 
+
+Select Type,Count(Type) as Records from AddressBook where Type='Family' Group by Type;
